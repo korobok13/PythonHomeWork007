68537eb83c76f3759fbe23b0b7b3995b57d11696
korobov_o <korobok13@yandex.ru>
2023-02-22 17:27:35 +0300

    072

--- HomeWorkPython072.py
+++ HomeWorkPython072.py
@@ -15,4 +15,12 @@
 #  5 10 15 20 25 30
 #  6 12 18 24 30 36 
 
+def print_operation_table(operation, num_rows = 6, num_columns = 6):
+    for i in range(1, num_rows + 1):
+        matrix = []
+        for j in range(1, num_columns + 1):
+            matrix.append(str(operation(i, j)))
+        print("  ".join(matrix))
+        
+
 print_operation_table(lambda x, y: x * y)
